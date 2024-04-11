c0affd787bf643f0e14e2612b5f146d7c45138e7
xTirian <tirianx@yahoo.com>
2024-04-11 20:15:23 -0300

    feat: repository

--- src/repositories/caraminhola.repository.ts
+++ src/repositories/caraminhola.repository.ts
@@ -2,4 +2,13 @@ import prisma from "../utils/prismaConfig";
 
 export class caraminholaRepository {
   public findFirst = prisma.caraminhola.findFirst;
+  public findMany = prisma.caraminhola.findMany;
+  public async getCaraminhola(id: string) {
+    return await prisma.caraminhola.findUnique({
+      where: { id },
+      include: {
+        Caraminhola_relation: true,
+      },
+    });
+  }
 }
--- src/repositories/caraminholaRelation.repository.ts
+++ src/repositories/caraminholaRelation.repository.ts
@@ -2,4 +2,5 @@ import prisma from "../utils/prismaConfig";
 
 export class caraminholaRelationReposirory {
   public findFirst = prisma.caraminhola_Relation.findFirst;
+  public findMany = prisma.caraminhola_Relation.findMany;
 }
